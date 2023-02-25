7bb6198c94bd769d012a0603585c2e779d673a04
Georgiy Savelyev <savelevgeo.post@gmail.com>
2023-02-25 20:04:51 +0000

    separate into function, use command line arguments

--- quiz/quiz.py
+++ quiz/quiz.py
@@ -4,23 +4,32 @@ Train cards from Quizlett
 print with 'small' layout and save as a .txt
 """
 
-with open(
-    'quizlett_deutch/trennbare Verben 1.txt',
-    encoding='utf-8'
-) as verbs_file:
-    verbs = verbs_file.read()
+def learn(file_path: str):
+    """
+    iterate over a file
+    """
+    with open(
+        file_path,
+        encoding='utf-8'
+    ) as verbs_file:
+        verbs = verbs_file.read()
 
-verbs_dict = {
-    verb.rpartition(' ')[2]: verb.rpartition(' ')[0][:-1]
-    for verb in verbs.splitlines()
-}
+    verbs_dict = {
+        verb.rpartition(' ')[2]: verb.rpartition(' ')[0][:-1]
+        for verb in verbs.splitlines()
+    }
 
-for verb_first, verb_form in verbs_dict.items():
-    print(verb_first)
-    attempt = input()
-
-    while attempt != verb_form:
-        print(verb_form)
+    for verb_first, verb_form in verbs_dict.items():
+        print(verb_first)
         attempt = input()
 
-    print('rightig')
+        while attempt != verb_form:
+            print(verb_form)
+            attempt = input()
+
+        print('rightig')
+
+if __name__ == '__main__':
+    import sys
+    path_to_file = sys.argv[1]
+    learn(path_to_file)
