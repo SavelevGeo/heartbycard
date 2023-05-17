ecc4ac28c4ad8e4ca6dfd6fce63e1e53defe9417
savelevgeo.post@gmail.com <savelevgeo.post@gmail.com>
2023-05-17 16:29:10 +0300

    make multipage

--- main.js
+++ main.js
@@ -1,15 +1,46 @@
+
 const app = Vue.createApp({
   data() {
     return {
-      fileNames: ['Verben mit präpostitonen.txt']
+      nodes: []
     }
+  },
+  created() {
+    fetch('https://api.github.com/repos/SavelevGeo/heartbycard/git/trees/main?recursive=1')
+      .then(resp => resp.json())
+      .then(json => {
+        this.nodes = json.tree
+          .filter(node => node.path.match(
+            /quiz\/.*\.txt/)
+          )
+          .map(node => {
+            const fileName = node.path
+              .split('/')[1]
+            
+            return {
+              stem: fileName.split('.')[0],
+              appLink: this.link(fileName),
+              dataLink: this.rawLink(fileName),
+              collapsed: true
+            }
+            
+          });
+          
+      })
   },
   methods: {
     link(fileName) {
       const fileNameEncoded = encodeURI(fileName)
       
       return `learn/?file=${fileNameEncoded}`
-    }
+    },
+    
+    rawLink(fileName) {
+      const fileNameEncoded = encodeURI(fileName)
+      
+      return `https://raw.githubusercontent.com/SavelevGeo/heartbycard/main/quiz/${fileNameEncoded}`
+    },
+    
   }
 });
 
