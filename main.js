79e48b960c8553bc82caf8d236c7c1c0b0b3aa0f
savelevgeo.post@gmail.com <savelevgeo.post@gmail.com>
2023-05-18 17:30:04 +0300

    reorganize card sets manipulation into classes

--- main.js
+++ main.js
@@ -1,46 +1,33 @@
+class cardsFile {
+  constructor(nodePath) {
+    this.fileName = nodePath.split('/')[1]
+    this.stem = this.fileName.split('.')[0];
+    this.collapsed = true;
+    
+    this._fileNameEncoded = encodeURI(this.fileName);
+    this.appLink = `learn/?file=${this._fileNameEncoded}`
+    this.rawLink = `https://raw.githubusercontent.com/SavelevGeo/heartbycard/main/quiz/${this._fileNameEncoded}`
+  }
+}
 
 const app = Vue.createApp({
   data() {
     return {
-      nodes: []
+      cardsFiles: []
     }
   },
   created() {
     fetch('https://api.github.com/repos/SavelevGeo/heartbycard/git/trees/main?recursive=1')
       .then(resp => resp.json())
       .then(json => {
-        this.nodes = json.tree
-          .filter(node => node.path.match(
-            /quiz\/.*\.txt/)
+        
+        this.cardsFiles = json.tree
+          .filter(node =>
+            node.path.match(/quiz\/.*\.txt/)
           )
-          .map(node => {
-            const fileName = node.path
-              .split('/')[1]
-            
-            return {
-              stem: fileName.split('.')[0],
-              appLink: this.link(fileName),
-              dataLink: this.rawLink(fileName),
-              collapsed: true
-            }
-            
-          });
-          
+          .map(node => new cardsFile(node.path)
+          );
       })
-  },
-  methods: {
-    link(fileName) {
-      const fileNameEncoded = encodeURI(fileName)
-      
-      return `learn/?file=${fileNameEncoded}`
-    },
-    
-    rawLink(fileName) {
-      const fileNameEncoded = encodeURI(fileName)
-      
-      return `https://raw.githubusercontent.com/SavelevGeo/heartbycard/main/quiz/${fileNameEncoded}`
-    },
-    
   }
 });
 
